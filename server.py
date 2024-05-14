d79211912c5cdcf73068d06767b78a0862bc1a98
gabrielolivra <gabriel.bernardino@urca.br>
2024-05-14 13:37:39 -0300

    finalizacao inicial servidor http

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import re
 
 PORT = 8000
 
@@ -7,22 +8,35 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_HEAD(self):
         return super().do_HEAD()
     
-    
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.send_header("Connection", "close")
+        self.send_header("Connection", "keep-alive")
+        self.send_header('keep-alive', 'timeout=1, max=10')
         self.end_headers()
-        # Replace 'Your custom message here' with the desired HTML content
+
         message = """
         <html>
         <body>
-        <h1>Welcome to your custom server!</h1>
-        <p>This is the content you requested.</p>
+        <h1>Bem vindo ao servidor de testes!</h1>
+        
+        <p>O conteudo vai ser exibido abaixo!</p>
         </body>
         </html>
         """
         self.wfile.write(message.encode())
+        msg = ""
+        print('Digite abaixo para exibir texto no cliente \nPara sair digite S')       
+        while True:
+            script = r'<script.*?</script>|<.*?javascript:.*?>|<.*?\\s+on.*?>'
+            texto = input('Mensagem: ')
+            if texto != 'S':
+                texto_tratado = re.sub(script, '', texto, flags=re.IGNORECASE | re.DOTALL)
+                result = msg + texto_tratado + '</br>'
+                self.wfile.write(result.encode())
+                self.wfile.flush()
+            else:
+                break
 
 with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print("Serving at port", PORT)
